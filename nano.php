e5a8b4b102fea42770fefcc05ad8b4ec7db73167
Andreas <info@azettl.net>
2017-08-02 16:32:53 +0530

    remove formatFunctionParameterValue method

--- nano.php
+++ nano.php
@@ -2,7 +2,7 @@
 /**
  * The php-nano-template class replaces placeholders in a string with values from an array.
  *
- * @package  nano
+ * @package  php-nano-template
  * @author   Andreas Zettl <info@azettl.net>
  * @see      https://github.com/azettl/php-nano-template
  */
@@ -140,7 +140,7 @@ final class nano{
           list(
             $sFormattedKey, 
             $mParam
-          )       = $this->getFunctionNameAndParameter($sKey);
+          )       = self::getFunctionNameAndParameter($sKey);
           $mValue = $aSearchIn[$sFormattedKey];
 
           if(is_string($mValue)) {
@@ -181,32 +181,13 @@ final class nano{
    * @param string $sKey   the key which needs to be replaced by the array value
    * @return array
    */
-  private function getFunctionNameAndParameter($sKey) : array
+  private static function getFunctionNameAndParameter($sKey) : array
   {
     preg_match_all("/\((.*?)\)/", $sKey, $aParam);
 
     return [
       str_replace($aParam[0][0], '', $sKey), 
-      $this->formatFunctionParameterValue($aParam[1][0])
+      preg_replace('/^["\'](.*)["\']$/m', '$1', $aParam[1][0]) // remove quotes
     ];
-  }
-
-
-  /**
-   * This method removes single and double quotes at the beginning and
-   * end of the passed string. 
-   *
-   * @param string $mValue   the value which needs to get formatted
-   * @return mixed           string if a parameter is available, null if not
-   */
-  private function formatFunctionParameterValue($mValue)
-  {
-    preg_match_all('/^(["\'])/', $mValue, $aParam);
-    
-    if($aParam && count($aParam[0]) >= 1){
-      $mValue = substr($mValue, 1, strlen($mValue)-2);
-    }
-
-    return $mValue;
   }
 }
